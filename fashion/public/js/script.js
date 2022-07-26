675a445b0190d0c24f69e189971f3e2e79e3458f
Amr-Gira <108944842+Amr-Gira@users.noreply.github.com>
2022-07-26 13:47:16 +0200

    Add files via upload

---
+++ fashion/public/js/script.js
@@ -0,0 +1,10 @@
+/*JSandJquery*/
+/*function show(){
+    document.getElementById("sss").style.display="block";
+}*/
+function tip(){
+    window.alert("ygtyg uybgyg ytvtyg yugytg ");    
+}
+function show(){
+    document.getElementById("ee").style.display="block";
+}
---
+++ fashion/resources/main.js
@@ -0,0 +1,20 @@
+document.write("<h1>Amr Mohamed</h1>");
+document.querySelector("h1").style.color = 'blue'; 
+
+console.log("%cElezero web school", "color: red; font-size: 40px;");
+
+console.group("group1");
+console.log("Amr");
+console.log("Mohamed");
+console.group("group2");
+console.log("AMR");
+console.log("MOHAMED2");
+console.group("group1");
+console.log("Amr");
+console.log("Mohamed");
+console.groupEnd();
+console.groupEnd();
+console.groupEnd();
+console.group("group2");
+console.log("OPEL");
+console.log("VECTRA");
